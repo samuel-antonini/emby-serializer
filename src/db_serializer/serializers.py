261c1d54ed3643179b84bae537644cf7154c647c
Samuel Antonini <39655468+samuel-antonini@users.noreply.github.com>
2021-09-24 16:49:16 -0300

    Serializes provider_ids as a dictionary with each provider as a key. (#3)

--- src/db_serializer/serializers.py
+++ src/db_serializer/serializers.py
@@ -19,11 +19,12 @@ class EmbyMovieSerializer(serializers.ModelSerializer):
     media_streams = EmbyMediaStreamSerializer(source="embymediastream_set", many=True)
     genres = EmbyMediaLinkedItemSerializer(read_only=True, many=True, source='linked_genres')
     studios = EmbyMediaLinkedItemSerializer(read_only=True, many=True, source='linked_studios')
+    providers = serializers.DictField(source='provider_ids_dict')
 
     class Meta:
         model = EmbyMediaItem
         fields = 'id', 'original_title', 'name', 'production_year', 'genres', 'studios', 'overview', 'community_rating', \
-                 'official_rating', 'critic_rating', 'provider_ids', 'data', 'images', 'filename', 'path', 'container', \
+                 'official_rating', 'critic_rating', 'providers', 'data', 'images', 'filename', 'path', 'container', \
                  'width', 'height', 'size', 'runtime_ticks', 'total_bitrate', 'date_created', 'date_modified', \
                  'media_streams'
 
@@ -31,20 +32,22 @@ class EmbyMovieSerializer(serializers.ModelSerializer):
 class EmbyTvShowSerializer(serializers.ModelSerializer):
     genres = EmbyMediaLinkedItemSerializer(read_only=True, many=True, source='linked_genres')
     studios = EmbyMediaLinkedItemSerializer(read_only=True, many=True, source='linked_studios')
+    providers = serializers.DictField(source='provider_ids_dict')
 
     class Meta:
         model = EmbyMediaItem
         fields = 'id', 'original_title', 'name', 'production_year', 'genres', 'studios', 'overview', \
-                 'community_rating', 'official_rating', 'critic_rating', 'provider_ids', 'data', 'images', 'path', \
+                 'community_rating', 'official_rating', 'critic_rating', 'providers', 'data', 'images', 'path', \
                  'date_created', 'date_modified'
 
 
 class EmbyEpisodeSerializer(serializers.ModelSerializer):
     media_streams = EmbyMediaStreamSerializer(source="embymediastream_set", many=True)
+    providers = serializers.DictField(source='provider_ids_dict')
 
     class Meta:
         model = EmbyMediaItem
         fields = 'id', 'original_title', 'name', 'production_year', 'overview', 'community_rating', \
-                 'official_rating', 'critic_rating', 'provider_ids', 'data', 'images', 'filename', 'path', 'container', \
+                 'official_rating', 'critic_rating', 'providers', 'data', 'images', 'filename', 'path', 'container', \
                  'width', 'height', 'size', 'runtime_ticks', 'total_bitrate', 'date_created', 'date_modified', \
                  'media_streams'
